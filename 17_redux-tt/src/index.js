86a641fb7aff6c659e207548821cef9fc71fb278
wji <69949121+baaaaaam@users.noreply.github.com>
2024-08-24 09:38:00 +0900

    Add files via upload

---
+++ 17_redux-tt/src/index.js
@@ -0,0 +1,15 @@
+import ReactDom from 'react-dom';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import './index.css';
+import App from './App';
+import rootReducer from './modules';
+
+const store = createStore(rootReducer,composeWithDevTools());
+
+ReactDom.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+document.getElementById('root'));
